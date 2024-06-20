e7cb02d48feba62e9a8ad19db17fb9bfd1f2560b
David Van Komen <david.van_komen.ctr@afrl.af.mil>
2024-06-20 19:33:26 +0000

    feat: matmul kernel started (not validated)

---
+++ src/kernel_mmm.hpp
@@ -0,0 +1,85 @@
+#pragma once
+
+#include "Eigen"
+#include "Kokkos_Core.hpp"
+#include "common.hpp"
+#include "kernel.hpp"
+#include "range.hpp"
+
+// just the traditional implementation, NOT FAST
+
+struct FunctorMMM_Host
+{
+    template<typename ViewsTuple, typename Index>
+    KOKKOS_FUNCTION void operator()(ViewsTuple views, const Index i, const Index j) const
+    {
+        // certainly want some eigen implementation as well
+        auto A = std::get<0>(views);
+        auto B = std::get<1>(views);
+        auto C = std::get<2>(views);
+
+        double sum = 0.0;
+
+        for (size_t k = 0; k < A.extent(1); k++)
+        {
+            sum += A(i, k) * B(k, j);
+        }
+
+        C(i, j) = sum;
+    }
+};
+
+struct FunctorMMM_Device
+{
+    tempate<typename ViewsTuple, typename Index> KOKKOS_FUNCTION void
+    operator()(ViewsTuple views, const Index i, const Index j) const
+    {
+        // maybe just straight up CuBLAS?
+        auto A = std::get<0>(views);
+        auto B = std::get<1>(views);
+        auto C = std::get<2>(views);
+
+        double sum = 0.0;
+
+        for (size_t k = 0; k < A.extent(1); k++)
+        {
+            sum += A(i, k) * B(k, j);
+        }
+
+        C(i, j) = sum;
+    }
+};
+
+
+template<typename... ParameterTypes>
+inline auto KernelMMM(KernelOptions& options, ParameterTypes&... data_views)
+{
+    auto name = "matrix-matrix multiply";
+
+    auto is_const = kernel_io_map(data_views...);
+
+    // WARNING, remapping loses const!
+    auto views_ = pack(data_views...);
+
+    auto views = std::make_tuple(
+      std::make_tuple(get_v(0, 0, views_), get_v(1, 0, views_), get_v(2, 0, views_)),
+      std::make_tuple(get_v(0, 1, views_), get_v(1, 1, views_), get_v(2, 1, views_)),
+      std::make_tuple(get_v(0, 2, views_), get_v(1, 2, views_), get_v(2, 2, views_)));
+
+    // get the extent based on the host view of the matrix
+    auto out = std::get<0>(std::get<0>(views));
+
+    // we'll assume that the user has properly set up the right matrix sizes, so the iterations are
+    // only in the out to store
+    unsigned long N = out.extent(0);
+    unsigned long M = out.extent(1);
+
+    auto extent = range_extent({ 0, 0 }, { N, M });
+
+    return Kernel<2, FunctorMMM_Host, FunctorMMM_Device, decltype(views), decltype(is_const)>(
+      name,
+      views,
+      is_const,
+      extent,
+      options);
+}
