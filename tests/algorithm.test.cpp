7c896e4b09e0a1393e6fe8e31f1206ca144cfecf
David Van Komen <david.van_komen.ctr@afrl.af.mil>
2024-06-24 18:50:16 +0000

    feat: algorithm-level unit tests

---
+++ tests/algorithm.test.cpp
@@ -0,0 +1,258 @@
+#include "algorithm.hpp"
+
+#include "Kokkos_Core.hpp"
+#include "common.hpp"
+#include "data.hpp"
+#include "data_transfers.hpp"
+#include "kernel_matvecmult.hpp"
+#include "kernel_vectordot.hpp"
+#include "view.hpp"
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
+#include <cstddef>
+#include <utility>
+
+typedef Kokkos::Cuda DeviceExecSpace;
+typedef Kokkos::RangePolicy<DeviceExecSpace> device_range_policy;
+typedef Kokkos::MDRangePolicy<DeviceExecSpace, Kokkos::Rank<2>> device_rank2_range_policy;
+
+TEST_CASE("Algorithm: Test No Dependencies, No Reordering")
+{
+    const size_t N        = 10;
+    const size_t M        = 20;
+    const bool reordering = false;
+
+    const DeviceSelector device = DeviceSelector::AUTO;
+
+    // execution options
+    KernelOptions options;
+    if (device != DeviceSelector::AUTO)
+        options = { { device } };
+    else
+        options = { { DeviceSelector::HOST, DeviceSelector::DEVICE } };
+
+    Kokkos::initialize();
+    {
+
+#include "helpers/algorithm_build_views.cpp"
+
+        // truth vectors for numeric computations
+        std::vector<double> c_truth(N);
+        std::vector<double> f_truth(N);
+
+        for (size_t i = 0; i < N; i++)
+        {
+            c_truth[i] = a[i] * b[i];
+            f_truth[i] = d[i] * e[i];
+            c[i]       = 0.0;
+            f[i]       = 0.0;
+        }
+
+        // first test is just a few VectorDot kernels that should work
+        auto k1 = KernelVectorDot(options, std::as_const(a_views), std::as_const(b_views), c_views);
+        auto k2 = KernelVectorDot(options, std::as_const(d_views), std::as_const(e_views), f_views);
+
+        auto kernels = pack(k1, k2);
+
+        Algorithm algo(kernels, data_views, reordering);
+
+        // do some checks on the algorithm's internals
+        REQUIRE(algo.reordering_ == reordering);
+
+        // check the size of detected inputs and outputs
+        REQUIRE(algo.inputs.size() == 4);
+        REQUIRE(algo.outputs.size() == 2);
+
+        // dependents is a vector of sets, for kernel set, we have nothing
+        for (auto& dep : algo.dependents)
+        {
+            REQUIRE(dep.size() == 0);
+        }
+        // depends_on is similar, it's the "prev" in a directional graph
+        for (auto& dep : algo.depends_on)
+        {
+            REQUIRE(dep.size() == 0);
+        }
+
+        // the algorithm "graph" is the simple graph used for traversal based on index.
+        // this index is the kernel ID, which is the order passed into the tuple.
+        // In particular, this is the KERNEL version of the dependencies
+        for (auto& data_node : algo.graph)
+        {
+            REQUIRE(data_node.prev.size() == 0);
+            REQUIRE(data_node.next.size() == 0);
+        }
+
+        // the data dependecy graph is a bit trickier to verify, since it stores everything
+        for (auto& data_dep : algo.data_graph.graph)
+        {
+            const size_t view_id = data_dep.first;
+            // views a, b, d, e (inputs)
+            if (view_id == 2 || view_id == 3 || view_id == 5 || view_id == 6)
+            {
+                REQUIRE(data_dep.second.prev.size() == 0);
+                REQUIRE(data_dep.second.next.size() == 1);
+            }
+            // views c and f (outputs)
+            else if (view_id == 4 || view_id == 7)
+            {
+                REQUIRE(data_dep.second.prev.size() == 2);
+                REQUIRE(data_dep.second.next.size() == 0);
+            }
+        }
+
+        // also check the number of chains!
+        // in this case, each of the two can either both be on host, one be on device, or both be on
+        // device. that makes 4.
+        REQUIRE(algo.kernel_chains.size() == 4);
+
+        // then set up our algorithm verification function to test both of our outputs!
+        algo.set_validation_function([&a, &b, &c, &d, &e, &f, &c_truth, &f_truth, &N]()
+        {
+            double abs_difference = 0.0;
+            for (size_t i = 0; i < N; i++)
+            {
+                abs_difference += std::abs(c[i] - c_truth[i]);
+                c[i] = 0.0;
+            };
+            REQUIRE_THAT(abs_difference, Catch::Matchers::WithinRel(0.0, 1.0e-10));
+
+            abs_difference = 0.0;
+            for (size_t i = 0; i < N; i++)
+            {
+                abs_difference += std::abs(f[i] - f_truth[i]);
+                f[i] = 0.0;
+            };
+            REQUIRE_THAT(abs_difference, Catch::Matchers::WithinRel(0.0, 1.0e-10));
+        });
+
+        printf("\nNow running algorithm...\n");
+        algo();
+        printf("\nFinished running algorithm!\n");
+
+        // DONE
+    }
+    Kokkos::finalize();
+}
+
+
+
+TEST_CASE("Algorithm: Test No Dependencies, W/ Reordering")
+{
+    const size_t N        = 10;
+    const size_t M        = 20;
+    const bool reordering = true;
+
+    const DeviceSelector device = DeviceSelector::AUTO;
+
+    // execution options
+    KernelOptions options;
+    if (device != DeviceSelector::AUTO)
+        options = { { device } };
+    else
+        options = { { DeviceSelector::HOST, DeviceSelector::DEVICE } };
+
+    Kokkos::initialize();
+    {
+
+#include "helpers/algorithm_build_views.cpp"
+
+        // truth vectors for numeric computations
+        std::vector<double> c_truth(N);
+        std::vector<double> f_truth(N);
+
+        for (size_t i = 0; i < N; i++)
+        {
+            c_truth[i] = a[i] * b[i];
+            f_truth[i] = d[i] * e[i];
+            c[i]       = 0.0;
+            f[i]       = 0.0;
+        }
+
+        // first test is just a few VectorDot kernels that should work
+        auto k1 = KernelVectorDot(options, std::as_const(a_views), std::as_const(b_views), c_views);
+        auto k2 = KernelVectorDot(options, std::as_const(d_views), std::as_const(e_views), f_views);
+
+        auto kernels = pack(k1, k2);
+
+        Algorithm algo(kernels, data_views, reordering);
+
+        // do some checks on the algorithm's internals
+        REQUIRE(algo.reordering_ == reordering);
+
+        // check the size of detected inputs and outputs
+        REQUIRE(algo.inputs.size() == 4);
+        REQUIRE(algo.outputs.size() == 2);
+
+        // dependents is a vector of sets, for kernel set, we have nothing
+        for (auto& dep : algo.dependents)
+        {
+            REQUIRE(dep.size() == 0);
+        }
+        // depends_on is similar, it's the "prev" in a directional graph
+        for (auto& dep : algo.depends_on)
+        {
+            REQUIRE(dep.size() == 0);
+        }
+
+        // the algorithm "graph" is the simple graph used for traversal based on index.
+        // this index is the kernel ID, which is the order passed into the tuple.
+        // In particular, this is the KERNEL version of the dependencies
+        for (auto& data_node : algo.graph)
+        {
+            REQUIRE(data_node.prev.size() == 0);
+            REQUIRE(data_node.next.size() == 0);
+        }
+
+        // the data dependecy graph is a bit trickier to verify, since it stores everything
+        for (auto& data_dep : algo.data_graph.graph)
+        {
+            const size_t view_id = data_dep.first;
+            // views a, b, d, e (inputs)
+            if (view_id == 2 || view_id == 3 || view_id == 5 || view_id == 6)
+            {
+                REQUIRE(data_dep.second.prev.size() == 0);
+                REQUIRE(data_dep.second.next.size() == 1);
+            }
+            // views c and f (outputs)
+            else if (view_id == 4 || view_id == 7)
+            {
+                REQUIRE(data_dep.second.prev.size() == 2);
+                REQUIRE(data_dep.second.next.size() == 0);
+            }
+        }
+
+        // also check the number of chains!
+        // in this case, each of the two can either both be on host, one be on device, or both be on
+        // device. that makes 8 since they can go in either order.
+        REQUIRE(algo.kernel_chains.size() == 8);
+
+        // then set up our algorithm verification function to test both of our outputs!
+        algo.set_validation_function([&a, &b, &c, &d, &e, &f, &c_truth, &f_truth, &N]()
+        {
+            double abs_difference = 0.0;
+            for (size_t i = 0; i < N; i++)
+            {
+                abs_difference += std::abs(c[i] - c_truth[i]);
+                c[i] = 0.0;
+            };
+            REQUIRE_THAT(abs_difference, Catch::Matchers::WithinRel(0.0, 1.0e-10));
+
+            abs_difference = 0.0;
+            for (size_t i = 0; i < N; i++)
+            {
+                abs_difference += std::abs(f[i] - f_truth[i]);
+                f[i] = 0.0;
+            };
+            REQUIRE_THAT(abs_difference, Catch::Matchers::WithinRel(0.0, 1.0e-10));
+        });
+
+        printf("\nNow running algorithm...\n");
+        algo();
+        printf("\nFinished running algorithm!\n");
+
+        algo.print_results();
+    }
+    Kokkos::finalize();
+}
---
+++ tests/helpers/algorithm_build_views.cpp
@@ -0,0 +1,83 @@
+/**
+ * @file algorithm_build_views.cpp
+ * @brief This file is just pure C++ that is to be **included** for building algorithm views
+ *
+ */
+
+// NOTE: THIS IS **NOT** VALID C++ TO BE COMPILED, THIS IS ONLY TO BE INCLUDED
+// TO AVOID REPEATING THINGS IN THE TEST CASES
+
+std::vector<double> a(N);
+std::vector<double> b(N);
+std::vector<double> c(N);
+std::vector<double> d(N);
+std::vector<double> e(N);
+std::vector<double> f(N);
+std::vector<double> g(N);
+std::vector<double> h(N);
+
+// a few matrices
+DynMatrix2D A(N, M);
+DynMatrix2D B(N, M);
+
+// then initialize
+std::iota(a.begin(), a.end(), 1.0);
+std::iota(b.begin(), b.end(), 2.0);
+std::iota(c.begin(), c.end(), 3.0);
+std::iota(d.begin(), d.end(), 4.0);
+std::iota(e.begin(), e.end(), 5.0);
+std::iota(f.begin(), f.end(), 6.0);
+std::iota(g.begin(), g.end(), 7.0);
+std::iota(h.begin(), h.end(), 8.0);
+
+int ij = 0;
+for (size_t i = 0; i < N; i++)
+    for (size_t j = 0; j < M; j++)
+    {
+        A(i, j) = static_cast<double>(ij++);
+        B(i, j) = static_cast<double>(ij++);
+    }
+
+constexpr auto data_names = std::make_tuple(HashedName<hash("A")>(),
+                                            HashedName<hash("B")>(),
+                                            HashedName<hash("a")>(),
+                                            HashedName<hash("b")>(),
+                                            HashedName<hash("c")>(),
+                                            HashedName<hash("d")>(),
+                                            HashedName<hash("e")>(),
+                                            HashedName<hash("f")>(),
+                                            HashedName<hash("g")>(),
+                                            HashedName<hash("h")>());
+
+printf("\nbuilding the views...\n");
+auto data_views = create_views(pack(A, B, a, b, c, d, e, f, g, h));
+
+printf("views built!\n");
+
+auto& A_views = std::get<find<hash("A")>(data_names)>(data_views);
+auto& B_views = std::get<find<hash("B")>(data_names)>(data_views);
+// vector views
+auto& a_views = std::get<find<hash("a")>(data_names)>(data_views);
+auto& b_views = std::get<find<hash("b")>(data_names)>(data_views);
+auto& c_views = std::get<find<hash("c")>(data_names)>(data_views);
+auto& d_views = std::get<find<hash("d")>(data_names)>(data_views);
+auto& e_views = std::get<find<hash("e")>(data_names)>(data_views);
+auto& f_views = std::get<find<hash("f")>(data_names)>(data_views);
+auto& g_views = std::get<find<hash("g")>(data_names)>(data_views);
+auto& h_views = std::get<find<hash("h")>(data_names)>(data_views);
+
+
+#if 0
+else if constexpr (chain_use == 1)
+{
+    // this kernel is the "original" testing with k1, k3, and k4 from the old main.cpp
+    auto k1 = KernelVectorDot(options, std::as_const(a_views), std::as_const(b_views), c_views);
+    auto k2 = KernelVectorDot(options, std::as_const(d_views), std::as_const(e_views), f_views);
+    auto k3 = KernelVectorDot(options, std::as_const(c_views), std::as_const(g_views), h_views);
+    // so, k2 is independent and k3 is dependent
+
+    auto kernels = pack(k1, k2, k3);
+}
+#endif
+
+// then we can set up the algorithm
